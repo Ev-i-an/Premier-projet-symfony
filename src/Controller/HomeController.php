5314a1715b172eacf2e8072e0718075f79c34052
mouganib <mouganib@gmail.com>
2024-03-27 23:03:50 +0100

    jsp

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController {
-    
+
+    #[Route("/", name: "home")]
+
+    function index (Request $request): Response {
+
+        // dd($request);
+        return new Response(' Bonjour ' . $request->query->get('name', 'Inconnu'));
+    }
+
 }
