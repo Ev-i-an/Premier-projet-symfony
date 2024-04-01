188cdd240a573abcb3a63e9f36cecad3501ba1ab
mouganib <mouganib@gmail.com>
2024-04-01 23:33:23 +0200

    tes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController {
+class HomeController extends AbstractController {
 
     #[Route("/", name: "home")]
 
